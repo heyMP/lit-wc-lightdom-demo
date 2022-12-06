d04356e82ce515e06cb6ddc66432285a9d099750
Michael Potter <mgp140@gmail.com>
2022-12-06 12:25:30 -0500

    added build dir

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import html from '@web/rollup-plugin-html';
+import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
+import { terser } from 'rollup-plugin-terser';
+import litcss from 'rollup-plugin-lit-css';
+
+export default {
+  input: 'demo/index.html',
+  output: {
+    entryFileNames: '[hash].js',
+    chunkFileNames: '[hash].js',
+    assetFileNames: '[hash][extname]',
+    format: 'es',
+    dir: 'dist',
+  },
+  preserveEntrySignatures: false,
+
+  plugins: [
+    /** Enable using HTML as rollup entrypoint */
+    html({
+      minify: true,
+    }),
+    litcss(),
+    /** Resolve bare module imports */
+    nodeResolve(),
+    /** Minify JS */
+    terser(),
+    /** Bundle assets references via import.meta.url */
+    importMetaAssets(),
+  ],
+};
+
