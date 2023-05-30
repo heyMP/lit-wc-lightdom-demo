0bbd0252cb87bb5f684eb4c71550eda03aa59b9e
Michael Potter <mgp140@gmail.com>
2023-05-30 17:17:43 -0400

    converting lightdom stylesheet to constructable

--- src/XList.ts
+++ src/XList.ts
@@ -1,11 +1,34 @@
 import { html, LitElement, PropertyValues } from 'lit';
 import { property } from 'lit/decorators.js';
 // @ts-ignore
-import lightdomStyles from './x-list-lightdom.css' assert { type: 'css'};
+import lightdomStyles from './x-list-lightdom.css' assert { type: 'css' };
+
+// Make a shared constructable stylesheet.
+// This will bethe cononical source of styles for this element's shadowroot
+// and lightdom.
+let styles = new CSSStyleSheet();
+// Read the original styleSheet that was created by a traditional <link>
+const styleSheet = Array.from(document.styleSheets).find(i => i.title === "x-list");
+// move all of the style from the original into the new constructable stylesheet
+if (styleSheet) {
+  for (const rule of Object.values(styleSheet.cssRules)) {
+    styles.insertRule(rule.cssText, styles.cssRules.length);
+  }
+  // Now adopt the constructable stylesheet to the document
+  document.adoptedStyleSheets = [...document.adoptedStyleSheets, styles];
+
+  // Now disabled the original lightdom style
+  styleSheet.disabled = true;
+  document.styleSheets.item(0)?.ownerNode?.remove();
+}
+else {
+  styles = lightdomStyles;
+  document.adoptedStyleSheets = [...document.adoptedStyleSheets, styles];
+}
 
 export class XList extends LitElement {
   static tag = `x-list`;
-  static styles = lightdomStyles;
+  static styles = styles;
 
   @property({ type: Array })
   public items = [];
@@ -16,29 +39,6 @@ export class XList extends LitElement {
   willUpdate(changed: PropertyValues<this>) {
     if (changed.has('done') && this.done) {
       this.done = this.done.map(i => Number(i));
-    }
-  }
-
-  connectedCallback() {
-    super.connectedCallback()
-    /**
-     * Append any lightdom styles for this element to the parent 
-     */
-    const root = this.parentNode?.getRootNode();
-    if (root) {
-      console.log(root);
-      // @ts-ignore
-      // const lightdomStylesStylesheet = lightdomStyles.styleSheet;
-      // // @ts-ignore
-      // const hasAdoptedStylesheet = [...root.adoptedStyleSheets].find(s => {
-      //   if (s.cssRules.length !== lightdomStylesStylesheet.cssRules.length) return false;
-      //   if (s.cssRules[0].cssText !== lightdomStylesStylesheet.cssRules[0].cssText) return false;
-      //   return true;
-      // });
-      // if (!hasAdoptedStylesheet) {
-      // @ts-ignore
-      root.adoptedStyleSheets = [...root.adoptedStyleSheets ?? [], lightdomStyles];
-      // }
     }
   }
 
